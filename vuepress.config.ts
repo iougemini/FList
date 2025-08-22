2bbf65497822046f1f79436a8183bc4afbca7a86
iougemini <163802753+iougemini@users.noreply.github.com>
2025-08-22 23:10:16 +0800

    Update vuepress.config.ts

--- vuepress.config.ts
+++ vuepress.config.ts
@@ -21,11 +21,11 @@ export default defineUserConfig({
   lang: 'zh-CN',
   public: `./public`,
   // 网站标题，标题颜色可在 src/client/css/main.css 中修改
-  title: 'FList',
+  title: 'Github List',
   // 网站的简介，有助于搜索引擎收录
   description: 'FList - 将 GitHub Releases 以类似网盘的形式展示在网页上，方便用户下载开源软件。 支持视频、音频、图片、PDF 等文件的在线预览。',
   // 页面 <head> 标签内添加的额外标签。 不要修改/logo.png可以替换掉这个文件，删除logo.png会导致构建出错。
-  head: [['link', { rel: 'icon', href: '/logo.png' }]],
+  head: [['link', { rel: 'icon', href: 'https://img.everyhub.top/i/2025/142b780bca129dd0.png' }]],
   // 页面预加载，所有其它页面所需的文件都会被预拉取。这对于小型站点来说是十分有帮助的，因为它会大大提升页面切换的速度。但是在你的网站有很多页面时不建议你这么做。
   // 简单来说就是，如果你的文件不多就可以打开这个选项，可以大大提高页面切换的速度，如果文件非常多就不建议打开。建议超过100个文件就不要打开这个选项。
   shouldPrefetch: true,
@@ -33,13 +33,13 @@ export default defineUserConfig({
   theme: FileList([
     {
       // 挂载路径
-      mountPath: "/KnapsackToGo4下载",
+      mountPath: "/Clash-verge-rev",
       // 文件解析器，这里使用githubReleasesFilesAnalysis,可以解析github的release文件
       analysis: githubReleasesFilesAnalysis({
         // 仓库所有者的用户名
-        user: "jianjianai",
+        user: "clash-verge-rev",
         // 仓库所有者的仓库名
-        repository: "KnapsackToGo4",
+        repository: "clash-verge-rev"
         // github 授权 Token, process.env.xxx  xxx 是环境变量名称。可以通过设置 githubToken 环境变量来配置
         authorizationToken: process.env.githubToken,
         // 分页大小，不懂得话就当作取最新的多少个标签吧。
@@ -47,58 +47,173 @@ export default defineUserConfig({
       }),
     },
     {
-      mountPath: "/",
-      analysis: githubReleasesFilesAnalysis({ user: "jianjianai", repository: "FList" , authorizationToken: process.env.githubToken,}),
+
+      mountPath: "/Mihomo-party",
+      //analysis: githubReleasesFilesAnalysis({ user: "jianjianai", repository: "FList" , authorizationToken: process.env.githubToken,}),
       // 下载代理配置,支持多个平台，参考:https://jjaw.cn/2024/8/3/flist-config-porxy/
       // 这个是为了解决github的国内下载慢的问题，和跨域问题，建议配置，不然pdf，txt，md等文件因为跨域无法预览
       // 如果你使用的不是 cloudflare Pages 部署需要删掉这一行，因为如果不是cloudflare Pages部署，这个代理是无法正常工作的
       downProxy: cloudflarePagesDownProxy(),
+      // 文件解析器，这里使用githubReleasesFilesAnalysis,可以解析github的release文件
+      analysis: githubReleasesFilesAnalysis({
+        // 仓库所有者的用户名
+        user: "mihomo-party-org",
+        // 仓库所有者的仓库名
+        repository: "mihomo-party"
+      }),
     },
     {
-      mountPath: "/",
+      // 挂载路径
+      mountPath: "/KaringX",
+      // 文件解析器，这里使用githubReleasesFilesAnalysis,可以解析github的release文件
+      analysis: githubReleasesFilesAnalysis({
+        // 仓库所有者的用户名
+        user: "KaringX",
+        // 仓库所有者的仓库名
+        repository: "karing"
+      }),
+    },
+    {
+      // 挂载路径
+      mountPath: "/FlClash",
+      // 文件解析器，这里使用githubReleasesFilesAnalysis,可以解析github的release文件,https://github.com/apernet/hysteria/releases
+      analysis: githubReleasesFilesAnalysis({
+        // 仓库所有者的用户名
+        user: "chen08209",
+        // 仓库所有者的仓库名
+        repository: "FlClash"
+      }),
+    },
+    {
+      // 挂载路径
+      mountPath: "/NetMount",
+      // 文件解析器，这里使用githubReleasesFilesAnalysis,可以解析github的release文件,https://github.com/apernet/hysteria/releases
+      analysis: githubReleasesFilesAnalysis({
+        // 仓库所有者的用户名
+        user: "VirtualHotBar",
+        // 仓库所有者的仓库名
+        repository: "NetMount"
+      }),
+    },
+    {
+      // 挂载路径
+      mountPath: "/V2rayN",
+      // 文件解析器，这里使用githubReleasesFilesAnalysis,可以解析github的release文件
+      analysis: githubReleasesFilesAnalysis({
+        // 仓库所有者的用户名
+        user: "2dust",
+        // 仓库所有者的仓库名
+        repository: "v2rayN"
+      }),
+    },
+    {
+      // 挂载路径
+      mountPath: "/Sing-box",
+      // 文件解析器，这里使用githubReleasesFilesAnalysis,可以解析github的release文件,https://github.com/SagerNet/sing-box
+      analysis: githubReleasesFilesAnalysis({
+        // 仓库所有者的用户名
+        user: "SagerNet",
+        // 仓库所有者的仓库名
+        repository: "sing-box"
+      }),
+    },
+    {
+      // 挂载路径
+      mountPath: "/Nekoray",
+      // 文件解析器，这里使用githubReleasesFilesAnalysis,可以解析github的release文件,https://github.com/MatsuriDayo/nekoray/releases
+      analysis: githubReleasesFilesAnalysis({
+        // 仓库所有者的用户名
+        user: "MatsuriDayo",
+        // 仓库所有者的仓库名
+        repository: "nekoray"
+      }),
+    },
+    {
+      // 挂载路径
+      mountPath: "/Xray-core",
+      // 文件解析器，这里使用githubReleasesFilesAnalysis,可以解析github的release文件
+      analysis: githubReleasesFilesAnalysis({
+        // 仓库所有者的用户名
+        user: "XTLS",
+        // 仓库所有者的仓库名
+        repository: "Xray-core"
+      }),
+    },
+    {
+      // 挂载路径
+      mountPath: "/V2ray-core",
+      // 文件解析器，这里使用githubReleasesFilesAnalysis,可以解析github的release文件,https://github.com/v2fly/v2ray-core
+      analysis: githubReleasesFilesAnalysis({
+        // 仓库所有者的用户名
+        user: "v2fly",
+        // 仓库所有者的仓库名
+        repository: "v2ray-core"
+      }),
+    },
+    {
+      // 挂载路径
+      mountPath: "/Hysteria",
+      // 文件解析器，这里使用githubReleasesFilesAnalysis,可以解析github的release文件,https://github.com/apernet/hysteria/releases
+      analysis: githubReleasesFilesAnalysis({
+        // 仓库所有者的用户名
+        user: "apernet",
+        // 仓库所有者的仓库名
+        repository: "hysteria"
+      }),
+    },
+//    {
+//      mountPath: "/",
+//      analysis: githubReleasesFilesAnalysis({ user: "jianjianai", repository: "FList" }),
+      // 下载代理配置,支持多个平台，参考:https://jjaw.cn/2024/8/3/flist-config-porxy/
+      // 这个是为了解决github的国内下载慢的问题，和跨域问题，建议配置，不然pdf，txt，md等文件因为跨域无法预览
+//      // 如果你使用的不是 cloudflare Pages 部署需要删掉这一行，因为如果不是cloudflare Pages部署，这个代理是无法正常工作的
+//      downProxy: cloudflarePagesDownProxy(),
+//    },
+    {
+      mountPath: "/Soft",
       // 这里使用 fileUrlTreeAnalysis 文件放到对应的文件路径中
       analysis: fileUrlTreeAnalysis({
-        "/test2/文件树-测试视频1.mp4": "https://github.com/jianjianai/FList/releases/download/root/test.video.2.1080p.webm",
-        "/文件树测试/文件树-测试视频1.mp4": "https://github.com/jianjianai/FList/releases/download/root/test.video.2.1080p.webm",
-        "/文件树-测试视频1.mp4": "https://github.com/jianjianai/FList/releases/download/root/test.video.2.1080p.webm"
+       "/.NET 6.0 Desktop Runtime/v6.0.33.exe": "https://download.visualstudio.microsoft.com/download/pr/3ebc1f91-a5ba-477e-9353-198fa4e13371/35f447d6820b078fd18523764a4f0213/windowsdesktop-runtime-6.0.33-win-x64.exe",
+       "/.NET 8.0 Desktop Runtime/v8.0.8.exe": "https://download.visualstudio.microsoft.com/download/pr/907765b0-2bf8-494e-93aa-5ef9553c5d68/a9308dc010617e6716c0e6abd53b05ce/windowsdesktop-runtime-8.0.8-win-x64.exe"
+//        "/文件树测试/文件树-测试视频1.mp4": "https://github.com/jianjianai/FList/releases/download/root/test.video.2.1080p.webm",
+//        "/文件树-测试视频1.mp4": "https://github.com/jianjianai/FList/releases/download/root/test.video.2.1080p.webm"
       }),
       downProxy: cloudflarePagesDownProxy(),//如果文件树地址下载比较慢，也可以配置代理
-    },
-    {
-      mountPath: "/huggingface测试",
-      analysis: huggingFaceDatasetsAnalysis({
-        userName: "Open-Orca",
-        datasetsName: "OpenOrca",
-        branchName: "main",
-        path: "/",
-        //最大深度,如果文件夹有很多层最大递归解析多少层，默认10
-        maxDeep: 3
-      }),
-    },
-    {
-      mountPath: "/gitee测试/发行版",
-      analysis: giteeReleasesFilesAnalysis({
-        user: "jja8",
-        repository: "flist-test",
-        direction: "desc"
-      })
-    },
-    {
-      mountPath: "/gitee测试/仓库",
-      analysis: giteeReposAnalysis({
-        user: "jja8",
-        repository: "flist-test"
-      }),
-    },
-    {
-      mountPath: "/ProgrammingVTuberLogos",
-      analysis: githubReposAnalysis({
-        user: "Aikoyori",
-        repository: "ProgrammingVTuberLogos",
-        authorizationToken: process.env.githubToken,
-      }),
-      downProxy: cloudflarePagesDownProxy()
-    },
+    }
+//    {
+//      mountPath: "/huggingface测试",
+//      analysis: huggingFaceDatasetsAnalysis({
+//        userName: "Open-Orca",
+//        datasetsName: "OpenOrca",
+//        branchName: "main",
+//        path: "/",
+//        //最大深度,如果文件夹有很多层最大递归解析多少层，默认10
+//        maxDeep: 3
+//      }),
+//    },
+//    {
+//      mountPath: "/gitee测试/发行版",
+//      analysis: giteeReleasesFilesAnalysis({
+//        user: "jja8",
+//       repository: "flist-test",
+//        direction: "desc"
+//      })
+//    },
+//    {
+//      mountPath: "/gitee测试/仓库",
+//      analysis: giteeReposAnalysis({
+//        user: "jja8",
+//        repository: "flist-test"
+//      }),
+//    },
+//    {
+//      mountPath: "/ProgrammingVTuberLogos",
+//      analysis: githubReposAnalysis({
+//        user: "Aikoyori",
+//        repository: "ProgrammingVTuberLogos",
+//      }),
+//      downProxy: cloudflarePagesDownProxy()
+//    },
     // ... 可以配置多个挂载路径和仓库，以此类推
   ])
 })
